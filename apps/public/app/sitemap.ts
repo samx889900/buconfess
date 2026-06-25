4532ddd6daf3a9a30cda31c66ba6e379d8e822a0
thevikramrajput <vikrammadhad@gmail.com>
2026-06-25 21:50:51 +0530

    feat(public): add roomie link to sitemap

--- apps/public/app/sitemap.ts
+++ apps/public/app/sitemap.ts
@@ -5,6 +5,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
   
   return [
     {
+      url: 'https://roomie.buconfess.in/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
